eccf17a742d3dc4fffc2e9392f28771a441d1892
Nicolò <107205784+Vetratoh@users.noreply.github.com>
2022-06-10 12:02:45 +0200

    Add files via upload

---
+++ mio.bat
@@ -0,0 +1,15 @@
+@echo off
+set/P risp=
+echo Ciao come stai?
+set/P risp=
+if [%risp%]==[bene] echo Grandioso sono felice per te!
+set/P risp=
+if [%risp%]==[male] echo Povero… 🙁
+set/P risp=
+echo sei maschio o femmina?
+set/P risp=
+if [%risp%]==[maschio] echo ok
+set/P risp=
+if [%risp%]==[femmina] echo ok
+set/P risp=
+pause
